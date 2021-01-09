d91b6de9de829a67a9b333061a0d09a6f8212728
Vytautas <vytautas.1717@gmail.com>
2021-01-09 22:46:23 +0200

    add  folder

---
+++ JavaScript/Context call bind aplly/index.js
@@ -0,0 +1,33 @@
+function hi(){
+   console.log(' Hi JavaScript', this) 
+}
+
+const person = {
+    name:'Maximus',
+    sayHi:hi
+}
+// same how person
+const person1 = new Object({
+    name:'Maxi',
+    sayHi:hi
+} )
+// person2 with person prototype
+const person2 = Object.create(person)
+console.log(person2.name)  //Maximus
+person2.name = 'John'
+console.log(person2.name)  //John
+
+//I can add prototype mehtod
+Object.prototype.hello = function(){
+    console.log(' Hello prototype')
+}
+
+
+const number= new Number('42')
+const number2= new Number(42)
+const number1= 42
+let x
+let bool =true
+let bool1 =new Boolean(true)
+const str='42'
+const str1=new String('42')
